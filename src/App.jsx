4b153a3413428ea541673818579a5e1f0398786b
Barun Debnath <barundebnath91@gmail.com>
2021-10-22 12:19:52 +0530

    CardActions added

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,23 @@ const App = () => {
                   image="https://source.unsplash.com/random"
                   title="Image title"
                 />
-                <CardContent className={classes.cardContent}></CardContent>
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media car. You can use this section to describe
+                    the content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                </CardActions>
               </Card>
             </Grid>
           </Grid>
