50ba3e601de664f78606648c5169c3ac2efdc161
Barun Debnath <barundebnath91@gmail.com>
2021-10-22 12:22:40 +0530

    Card CSS styling added

--- src/styles.js
+++ src/styles.js
@@ -15,6 +15,20 @@ const useStyles = makeStyles((theme) => ({
   buttons: {
     marginTop: "40px",
   },
+  cardGrid: {
+    padding: "20px 0",
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", //16:9 aspect ratio
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
 }));
 
 export default useStyles;
